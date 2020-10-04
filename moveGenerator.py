fb17e6e58f2dd92315608d994e14033cd964ecf8
thdossch <dossche.thor@gmail.com>
2020-10-04 18:51:12 +0200

    added move generator

--- moveGenerator.py
+++ moveGenerator.py
@@ -3,7 +3,6 @@ class MoveGenerator:
     def __init__(self):
         '''
         Class that can generate moves based on a player and the last placed card of the game.
-
         '''
         self.special_cards = {7: (lambda card: card.rank < 7)}
 
@@ -21,4 +20,23 @@ class MoveGenerator:
         return list(filter(lambda card: last_move.rank <= card.rank and \
                            len(player.get_cards_of_rank(card.rank)) >= last_move.amount, player.cards))
 
-        
+    def generate_possible_moves(self, player, last_move):
+        valid_cards = list(filter(lambda card: last_move.rank <= card.rank, player.cards))
+        ranks = self.get_all_ranks(valid_cards)
+        jokers = list(filter(lambda card: card.rank == self.joker), player.cards)
+
+        possible_moves = []
+        for rank in ranks:
+            moves = self.get_all_combinations(list(filter(lambda card: card.rank == rank, valid_cards)), jokers)
+            possible_moves.append(moves)
+        return possible_moves
+
+    def get_all_ranks(self, cards):
+        ranks = []
+        for card in cards:
+            if card.rank not in ranks and rank != self.joker:
+                ranks.append(rank)
+        return ranks
+
+    def get_all_combinations(self, cards, jokers):
+        return [ (cards + jokers)[0:i+1] for i in range(len(cards + jokers)) ]
