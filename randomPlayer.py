f6f9b25ed4b34096663d164ddcf0e05e777fde7d
thdossch <dossche.thor@gmail.com>
2020-10-04 18:52:33 +0200

    added basicPlayer again and moved generator things to randomPlayer

--- randomPlayer.py
+++ randomPlayer.py
@@ -2,6 +2,7 @@ from player import Player
 from skip import Skip
 from card import Card
 from move import Move
+from moveGenerator import MoveGenerator
 
 class BasicPlayer(Player):
     '''
@@ -10,18 +11,17 @@ class BasicPlayer(Player):
 
     def play(self, last_move):
         move = None
-        possible_moves = []
+        possible_cards = []
         # Get possible moves
         # Check if the move is the first of the round 
         if last_move.is_round_start():
-            possible_moves = self.cards
+            possible_cards = self.cards
         else:
-            possible_moves = list(filter(lambda card: last_move.rank <= card.rank and \
-                                         len(self.get_cards_of_rank(card.rank)) >= last_move.amount, self.cards))
+            possible_cards = MoveGenerator().possible_cards(self, last_move)
                 
-        if possible_moves:
+        if possible_cards:
             # Get the cards with the same rank
-            cards_to_play = self.get_cards_of_rank(possible_moves[0].rank)
+            cards_to_play = self.get_cards_of_rank(possible_cards[0].rank)
             # Update own cards
             self.cards = list(filter(lambda card: card not in cards_to_play, self.cards))
             # Create the move
@@ -32,14 +32,4 @@ class BasicPlayer(Player):
         print(f"{self.name} plays {move}")
         return move
 
-    def get_cards_of_rank(self, rank):
-        '''
-        Function that returns all cards of a given rank
-
-        Parameters: 
-            rank: int
-        Returns:
-            cards: [Card]
-        '''
-        return list(filter(lambda card: card.rank == rank, self.cards))
         
